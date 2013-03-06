892855e064ae5a60709d428d45d50d9082835bfa
Craig Walker <github@softcraft.ca>
2013-03-06 13:33:56 -0700

    Parens instead of braces.
    Why bother?

--- src/test/scala/org/functionalkoans/forscala/AboutLists.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutLists.scala
@@ -89,8 +89,8 @@ class AboutLists extends KoanSuite {
 
   koan("Functions over lists can use () instead of {}") {
     val a = List(1, 2, 3)
-    a.map(_ * 2) should equal(List(__, __, __))
-    a.filter(_ % 2 != 0) should equal(List(__, __))
+    a.map(_ * 2) should equal(List(2, 4, 6))
+    a.filter(_ % 2 != 0) should equal(List(1, 3))
   }
 
   koan("Lists can be 'reduced' with a mathematical operation") {
