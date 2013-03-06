658e65c0ba026d2cb473c4e2adb24aa0379c86a2
Craig Walker <github@softcraft.ca>
2013-03-06 13:40:28 -0700

    FoldLeft
    same as ReduceLeft but with an explicit start

--- src/test/scala/org/functionalkoans/forscala/AboutLists.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutLists.scala
@@ -104,10 +104,10 @@ class AboutLists extends KoanSuite {
   koan("Foldleft is like reduce, but with an explicit starting value") {
     val a = List(1, 3, 5, 7)
     // NOTE: foldLeft uses a form called currying that we will explore later
-    a.foldLeft(0)(_ + _) should equal(__)
-    a.foldLeft(10)(_ + _) should equal(__)
-    a.foldLeft(1)(_ * _) should equal(__)
-    a.foldLeft(0)(_ * _) should equal(__)
+    a.foldLeft(0)(_ + _) should equal(16)
+    a.foldLeft(10)(_ + _) should equal(26)
+    a.foldLeft(1)(_ * _) should equal(105)
+    a.foldLeft(0)(_ * _) should equal(0)
   }
 
   koan("You can create a list from a range") {
