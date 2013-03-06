476700f6c34ccc92c404c1f6ba2e3352879efe63
Craig Walker <github@softcraft.ca>
2013-03-06 13:39:50 -0700

    ReduceLeft
    apply operator to next element and results of previous op (or first
    element)

--- src/test/scala/org/functionalkoans/forscala/AboutLists.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutLists.scala
@@ -96,8 +96,8 @@ class AboutLists extends KoanSuite {
   koan("Lists can be 'reduced' with a mathematical operation") {
     val a = List(1, 3, 5, 7)
     // note the two _s below indicate the first and second args respectively
-    a.reduceLeft(_ + _) should equal(__)
-    a.reduceLeft(_ * _) should equal(__)
+    a.reduceLeft(_ + _) should equal(16)
+    a.reduceLeft(_ * _) should equal(105)
   }
 
 
