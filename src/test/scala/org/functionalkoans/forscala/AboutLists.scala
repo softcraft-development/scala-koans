68b862c5897a46ed490da8d6da049c96a52abe51
Craig Walker <github@softcraft.ca>
2013-03-06 13:29:17 -0700

    Index lookup for lists
    Also: throws a java.lang.IndexOutOfBoundsException, even for negative
    numbers

--- src/test/scala/org/functionalkoans/forscala/AboutLists.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutLists.scala
@@ -43,9 +43,9 @@ class AboutLists extends KoanSuite {
 
   koan("Lists can accessed at random") {
     val a = List(1, 3, 5, 7, 9)
-    a(0) should equal(__)
-    a(2) should equal(__)
-    a(4) should equal(__)
+    a(0) should equal(1)
+    a(2) should equal(5)
+    a(4) should equal(9)
 
     intercept[IndexOutOfBoundsException] {
       println(a(5))
