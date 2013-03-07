1868778e84db37e8e9afbb578d00f62d403c0e54
Craig Walker <github@softcraft.ca>
2013-03-07 10:56:17 -0700

    Removing lists of keys

--- src/test/scala/org/functionalkoans/forscala/AboutMutableMaps.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutMutableMaps.scala
@@ -42,8 +42,8 @@ class AboutMutableMaps extends KoanSuite {
   koan("Mutable maps can have Lists of elements removed") {
     val myMap = mutable.Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
     myMap --= List("IA", "OH")
-    myMap contains "OH" should be(__)
-    myMap.size should be(__)
+    myMap contains "OH" should be(false)
+    myMap.size should be(2)
   }
 
   koan("Mutable maps can be cleared") {
