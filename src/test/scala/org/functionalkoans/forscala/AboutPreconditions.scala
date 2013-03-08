d508d9a28b7dbf70f38fb3d5f1493535d8d15d59
Craig Walker <github@softcraft.ca>
2013-03-07 17:07:14 -0700

    Preconditions

--- src/test/scala/org/functionalkoans/forscala/AboutPreconditions.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutPreconditions.scala
@@ -7,6 +7,7 @@ class AboutPreconditions extends KoanSuite {
   class WithParameterRequirement(val myValue: Int) {
     require(myValue != 0)
 
+    // Auxilliary constructor
     def this(someValue: String) {
       this (1)
     }
@@ -14,8 +15,8 @@ class AboutPreconditions extends KoanSuite {
 
   // Instruction: use Intercept to catch the type of exception thrown by an invalid precondition
   koan("On precondition violation, intercept expects type of exception thrown") {
-    intercept[___] {
-      val myInstance = new WithParameterRequirement("")
+    intercept[IllegalArgumentException] {
+      val myInstance = new WithParameterRequirement(0)
     }
   }
 }
